f5e41d52167d261e0cafaca1298aee6bf35fe0b4
EdGemmill <edward.gemmill@gmail.com>
2022-09-08 15:19:22 +0100

    adding ticket 4

--- controllers/games.controllers.js
+++ controllers/games.controllers.js
@@ -1,7 +1,20 @@
-const { selectCategories } = require(`../models/games.models`);
+const {
+  selectCategories,
+  selectReviewById,
+} = require(`../models/games.models`);
 
 exports.getCategories = (req, res) => {
   selectCategories().then((data) => {
     res.status(200).send({ categories: data });
   });
+};
+
+exports.getReviewById = (req, res, next) => {
+  selectReviewById(req.params.review_id)
+    .then((data) => {
+      res.status(200).send({ review: data });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
